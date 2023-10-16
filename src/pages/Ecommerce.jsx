9a6955fcce7730e0e943361101a17b2e20d934a5
Kacy Phan <kacyphann@gmail.com>
2023-10-16 16:21:40 -0700

    feat: add sparkine to revenue updates

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,8 +9,15 @@ import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropd
 import { useStateContext } from '../contexts/ContextProvider';
 import product9 from '../data/product9.jpg';
 
+const DropDown = ({ currentMode }) => {
+    <div classsName="w-28 border-1 border-color px-2 py-1 rounded-md">
+        <DropDownListComponent id="time" fields={{ text: 'Time', value: 'ID' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
+
+    </div>;
+};
+
 const Ecommerce = () => {
-    const { currentColor } = useStateContext();
+    const { currentColor, currentMode } = useStateContext();
 
     return (
         <div className="mt-24">
@@ -57,6 +64,76 @@ const Ecommerce = () => {
                             <p className="text-sm text-gray-400  mt-1">{item.title}</p>
                         </div>
                     ))}
+                </div>
+            </div>
+            <div className="flex gap-10 flex-wrap justify-center">
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+                    <div className="flex justify-between">
+                        <p className="font-semibold text-xl">Revenue Updates</p>
+                        <div className="flex items-center gap-4">
+                            <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                                <span>
+                                    {/* <GoPrimitiveDot /> */}
+                                </span>
+                                <span>Expense</span>
+                            </p>
+                            <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                                <span>
+                                    {/* <GoPrimitiveDot /> */}
+                                </span>
+                                <span>Budget</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-10 flex gap-10 flex-wrap justify-center">
+                        <div className="border-r-1 border-color m-4 pr-10">
+                            <div>
+                                <p>
+                                    <span className="text-3xl font-semibold">$93,438</span>
+                                    <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                                        23%
+                                    </span>
+                                </p>
+                                <p className="text-gray-500 mt-1">Budget</p>
+                            </div>
+                            <div className="mt-8">
+                                <p className="text-3xl font-semibold">$48,487</p>
+                                <p className="text-gray-500 mt-1">Expense</p>
+                            </div>
+
+                            <div className="mt-5">
+                                <SparkLine currentColor={currentColor} id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} />
+                            </div>
+                            <div className="mt-10">
+                                <Button
+                                    color="white"
+                                    bgColor={currentColor}
+                                    text="Download Report"
+                                    borderRadius="10px"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <Stacked currentMode={currentMode} width="320px" height="360px" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className="rounded-2xl md:w-400 p-4 m-3"
+                        style={{ backgroundColor: currentColor }}
+                    >
+                        <div className="flex justify-between items-center">
+                            <p className="font-semibold text-white text-2xl">Earnings</p>
+
+                            <div>
+                                <p className="text-2xl text-white font-semibold mt-8">$63,488.78</p>
+                                <p className="text-gray-200">Monthly revenue</p>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
         </div>
