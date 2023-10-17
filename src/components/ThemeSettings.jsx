2e61d09beccfadf228ee0f449ea2af4ec77dd779
Kacy Phan <kacyphann@gmail.com>
2023-10-16 19:50:11 -0700

    feat: add dark mode to page

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
 import { BsCheck } from 'react-icons/bs';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import { themeColors } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -10,7 +11,7 @@ const ThemeSettings = () => {
 
     return (
         <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-            <div className="float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52]  w-400">
+            <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400">
                 <div className="flex justify-between items-center p-4 ml-4">
                     <p className="font-semibold text-lg">Settings</p>
                     <button
@@ -21,9 +22,10 @@ const ThemeSettings = () => {
                     >
                         <MdOutlineCancel />
                     </button>
+
                 </div>
                 <div className="flex-col border-t-1 border-color p-4 ml-4">
-                    <p className="font-semibold text-x">Theme Option</p>
+                    <p className="font-semibold text-xl ">Theme Option</p>
 
                     <div className="mt-4">
                         <input
@@ -35,13 +37,29 @@ const ThemeSettings = () => {
                             onChange={setMode}
                             checked={currentMode === 'Light'}
                         />
+                        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                        <label htmlFor="light" className="ml-2 text-md cursor-pointer">
+                            Light
+                        </label>
+                    </div>
+                    <div className="mt-2">
+                        <input
+                            type="radio"
+                            id="dark"
+                            name="theme"
+                            value="Dark"
+                            onChange={setMode}
+                            className="cursor-pointer"
+                            checked={currentMode === 'Dark'}
+                        />
+                        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
                         <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
                             Dark
                         </label>
                     </div>
                 </div>
                 <div className="p-4 border-t-1 border-color ml-4">
-                    <p className="font-semibold text-xl">Theme Color</p>
+                    <p className="font-semibold text-xl ">Theme Colors</p>
                     <div className="flex gap-3">
                         {themeColors.map((item, index) => (
                             <TooltipComponent key={index} content={item.name} position="TopCenter">
