60e91e48a23cea5144d0bb95d5521bb82b527fe0
Kacy Phan <kacyphann@gmail.com>
2023-10-17 15:17:09 -0700

    feat: add financial hilo

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, Inject, AccumulationLegend, AccumulationDataLabel, AccumulationTooltip, PyramidSeries, AccumulationSelection } from '@syncfusion/ej2-react-charts';
+
+import { PyramidData } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { ChartsHeader } from '../../components';
 
 const Pyramid = () => {
-    return (
-        <div>Pyramid</div>
-    )
-}
+    const { currentMode } = useStateContext();
 
-export default Pyramid
+    const Pyramid = () => {
+        return (
+            <div>Pyramid</div>
+        );
+    };
+};
+export default Pyramid;
